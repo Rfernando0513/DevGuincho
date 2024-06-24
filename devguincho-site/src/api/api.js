07cedb4435f4de989b7c5f223324ea92008a6b87
Rfernando <rfernando0513@gmail.com>
2024-06-23 23:11:36 -0300

    url api

--- devguincho-site/src/api/api.js
+++ devguincho-site/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://json-server-devguincho.vercel.app/comments'
+    baseURL: 'https://json-server-devguincho.vercel.app/'
 })
 
 
